8b697669de0ee9076de8a08b22cd2ebfeb092604
vermorth <vermorth@protonmail.com>
2021-04-08 17:27:48 +0800

    Repair the extra \0
    Signed-off-by: vermorth <vermorth@protonmail.com>

--- infra/http/reply.cc
+++ infra/http/reply.cc
@@ -6,22 +6,22 @@ namespace draft {
 namespace http {
 
 namespace status_strings {
-const char ok[] = "HTTP/1.1 200 OK\r\n";
-const char created[] = "HTTP/1.1 201 Created\r\n";
-const char accepted[] = "HTTP/1.1 202 Accepted\r\n";
-const char no_content[] = "HTTP/1.1 204 No Content\r\n";
-const char multiple_choices[] = "HTTP/1.1 300 Multiple Choices\r\n";
-const char moved_permanently[] = "HTTP/1.1 301 Moved Permanently\r\n";
-const char moved_temporarily[] = "HTTP/1.1 302 Moved Temporarily\r\n";
-const char not_modified[] = "HTTP/1.1 304 Not Modified\r\n";
-const char bad_request[] = "HTTP/1.1 400 Bad Request\r\n";
-const char unauthorized[] = "HTTP/1.1 401 Unauthorized\r\n";
-const char forbidden[] = "HTTP/1.1 403 Forbidden\r\n";
-const char not_found[] = "HTTP/1.1 404 Not Found\r\n";
-const char internal_server_error[] = "HTTP/1.1 500 Internal Server Error\r\n";
-const char not_implemented[] = "HTTP/1.1 501 Not Implemented\r\n";
-const char bad_gateway[] = "HTTP/1.1 502 Bad Gateway\r\n";
-const char service_unavailable[] = "HTTP/1.1 503 Service Unavailable\r\n";
+const std::string ok = "HTTP/1.1 200 OK\r\n";
+const std::string created = "HTTP/1.1 201 Created\r\n";
+const std::string accepted = "HTTP/1.1 202 Accepted\r\n";
+const std::string no_content = "HTTP/1.1 204 No Content\r\n";
+const std::string multiple_choices = "HTTP/1.1 300 Multiple Choices\r\n";
+const std::string moved_permanently = "HTTP/1.1 301 Moved Permanently\r\n";
+const std::string moved_temporarily = "HTTP/1.1 302 Moved Temporarily\r\n";
+const std::string not_modified = "HTTP/1.1 304 Not Modified\r\n";
+const std::string bad_request = "HTTP/1.1 400 Bad Request\r\n";
+const std::string unauthorized = "HTTP/1.1 401 Unauthorized\r\n";
+const std::string forbidden = "HTTP/1.1 403 Forbidden\r\n";
+const std::string not_found = "HTTP/1.1 404 Not Found\r\n";
+const std::string internal_server_error = "HTTP/1.1 500 Internal Server Error\r\n";
+const std::string not_implemented = "HTTP/1.1 501 Not Implemented\r\n";
+const std::string bad_gateway = "HTTP/1.1 502 Bad Gateway\r\n";
+const std::string service_unavailable = "HTTP/1.1 503 Service Unavailable\r\n";
 
 asio::const_buffer ToBuffers(StatusCode status) {
     switch (status) {
