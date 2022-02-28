fb7f407583e1a5c93cd3c05bb181c74397b11f88
minghaocong <minghaocong@tencent.com>
2022-03-01 00:50:43 +0800

    Rename liburing-2.1 to liburing
    Signed-off-by: minghaocong <minghaocong@tencent.com>

--- 3rdparty/liburing.cmake
+++ 3rdparty/liburing.cmake
@@ -2,34 +2,35 @@ include(ExternalProject)
 file(GLOB SRC_FILES_LIBURING "liburing/src/*.c")
 
 
-file(MAKE_DIRECTORY ${LIBURING_CONFIGURE_INCLUDE_PATH}/liburing)
-file(TOUCH ${LIBURING_CONFIGURE_INCLUDE_PATH}/liburing/config-host.h)
+set(LIBURING_CONFIGURE_PREIFX_DIR "${CMAKE_BINARY_DIR}/liburing-configure-prefix")
+file(MAKE_DIRECTORY ${LIBURING_CONFIGURE_PREIFX_DIR})
 
 ExternalProject_Add(
-        liburing-install
-        SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/liburing
-        PREFIX ${CMAKE_BINARY_DIR}/_deps
-        CONFIGURE_COMMAND chmod +x ${CMAKE_CURRENT_SOURCE_DIR}/liburing/configure && ${CMAKE_CURRENT_SOURCE_DIR}/liburing/configure --prefix=${CMAKE_BINARY_DIR}/_deps/liburing/install
-        INSTALL_COMMAND cmake -E echo "Skipping install step."
-        BUILD_COMMAND cmake -E echo "Skipping build step.")
+    liburing-configure
+    BUILD_IN_SOURCE 1
+    SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/liburing
+    PREFIX ${LIBURING_CONFIGURE_PREIFX_DIR}
+    CONFIGURE_COMMAND chmod +x ${CMAKE_CURRENT_SOURCE_DIR}/liburing/configure && ${CMAKE_CURRENT_SOURCE_DIR}/liburing/configure --prefix=${LIBURING_CONFIGURE_PREIFX_DIR}
+    INSTALL_COMMAND cmake -E echo "Skipping install step."
+    BUILD_COMMAND cmake -E echo "Skipping build step.")
 
 add_library(
-        uring
-        ${SRC_FILES_LIBURING})
+    uring
+    ${SRC_FILES_LIBURING})
 
 add_dependencies(
-        uring
-        liburing-install)
+    uring
+    liburing-configure)
 
 target_compile_options(
-        uring
-        PRIVATE
-        -include ${LIBURING_CONFIGURE_INCLUDE_PATH}/liburing/config-host.h -Wall -Wextra -Wno-unused-parameter -Wno-sign-compare)
+    uring
+    PRIVATE
+    -D_GNU_SOURCE -include ${CMAKE_CURRENT_SOURCE_DIR}/liburing/config-host.h -Wall -Wextra -Wno-unused-parameter -Wno-sign-compare)
 
 target_include_directories(
-        uring
-        PUBLIC
-        liburing/src/include liburing/src/include/liburing ${LIBURING_CONFIGURE_INCLUDE_PATH} ${LIBURING_BUILD_PATH})
-
-list(APPEND DEPS_LIST_LIBRARIES "uring")
+    uring
+    PUBLIC
+    ${CMAKE_CURRENT_SOURCE_DIR}/liburing
+    ${CMAKE_CURRENT_SOURCE_DIR}/liburing/src/include
+    ${CMAKE_CURRENT_SOURCE_DIR}/liburing/src/include/liburing)
 
